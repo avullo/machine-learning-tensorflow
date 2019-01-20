d9499298197a3206a01c371fd165947d9965bf5e
Alessandro Vullo <avullo@ebi.ac.uk>
2019-01-20 18:20:46 +0000

    Test HDF5 dataset creation with mini list of PDB IDs.

--- protstruct/protstruct/tests/test_dataset.py
+++ protstruct/protstruct/tests/test_dataset.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 
-from protstruct.dataset import get_pdb_file, extract_chain_data, process_pdbs
+from protstruct.dataset import * # get_pdb_file, extract_chain_data, process_pdbs
 from protstruct.config import ROOT_DIR, PDB_DIR
 
 import os
+import tempfile
 import numpy as np
 import tensorflow as tf
 
@@ -18,14 +19,14 @@ class TestCreateInputFile(TestCase):
         pdb_list = create_input_list(os.path.join(ROOT_DIR, 'data', 'minicullpdb'))
         self.assertEqual(len(pdb_list), 3)
 
-        self.assertEqual(pdb_list[0][0], '12as')
-        self.assertEqual(pdb_list[0][1], 'a')
+        self.assertEqual(pdb_list[0][0], '12AS')
+        self.assertEqual(pdb_list[0][1], 'A')
 
-        self.assertEqual(pdb_list[1][0], '16vp')
-        self.assertEqual(pdb_list[1][1], 'a')
+        self.assertEqual(pdb_list[1][0], '16VP')
+        self.assertEqual(pdb_list[1][1], 'A')
 
-        self.assertEqual(pdb_list[2][0], '1a0i')
-        self.assertEqual(pdb_list[2][1], 'a')
+        self.assertEqual(pdb_list[2][0], '1A0I')
+        self.assertEqual(pdb_list[2][1], 'A')
 
 class TestGetPDBFile(TestCase):
     """
@@ -62,20 +63,55 @@ class TestExtractChain(TestCase):
         self.assertEqual( len(chain), 328 )
 
         residue_info = chain[53]
-        self.assertEqual(residue_info['aa'], 'V')
-        self.assertEqual(residue_info['ss'], 'E')
+        self.assertEqual(residue_info['AA'], 'V')
+        self.assertEqual(residue_info['SS'], 'E')
         # see http://prowl.rockefeller.edu/aainfo/volume.htm
         # for residues surface areas
-        self.assertTrue(abs(residue_info['sa'] - 7.0 / 155) <= 1e-2)
-        self.assertEqual(residue_info['phi'], -113.0)
-        self.assertEqual(residue_info['psi'], 137.0)
+        self.assertTrue(abs(residue_info['SA'] - 7.0 / 155) <= 1e-2)
+        self.assertEqual(residue_info['Phi'], -113.0)
+        self.assertEqual(residue_info['Psi'], 137.0)
 
         residue_info = chain[299]
-        self.assertEqual(residue_info['aa'], 'L')
-        self.assertEqual(residue_info['ss'], 'H')
-        self.assertTrue(abs(residue_info['sa'] - 2.0 / 170) <= 1e-2)
-        self.assertEqual(residue_info['phi'], -76.3)
-        self.assertEqual(residue_info['psi'], -38.4)
+        self.assertEqual(residue_info['AA'], 'L')
+        self.assertEqual(residue_info['SS'], 'H')
+        self.assertTrue(abs(residue_info['SA'] - 2.0 / 170) <= 1e-2)
+        self.assertEqual(residue_info['Phi'], -76.3)
+        self.assertEqual(residue_info['Psi'], -38.4)
 
         tf.gfile.Remove( filename )
 
+class TestProcessPDBs(TestCase):
+    """
+    Test routine to process multiple PDB chains
+    """
+
+    def test_process_pdbs(self):
+        pdb_list = create_input_list(os.path.join(ROOT_DIR, 'data', 'minicullpdb'))
+
+        h5fname = os.path.join(ROOT_DIR, 'data', next(tempfile._get_candidate_names()) + '.h5')
+        process_pdbs(pdb_list, h5fname)
+        self.assertTrue( tf.gfile.Exists( h5fname ) )
+
+        # check file content
+        with h5py.File(h5fname, 'r') as f:
+            with self.assertRaises(Exception) as context:
+                f['12AS/B']
+
+            chain = f['12AS/A']
+            aa = np.array(chain['AA'])[0].decode('ascii')
+            ss = np.array(chain['SS'])[0].decode('ascii')
+            sa = np.array(chain['SA'])
+
+            self.assertEqual(aa, 'AYIAKQRQISFVKSHFSRQLEERLGLIEVQAPILSRVGDGTQDNLSGAEKAVQVKVKALPDAQFEVVHSLAKWKRQTLGQHDFSAGEGLYTHMKALRPDEDRLSPLHSVYVDQWDWERVMGDGERQFSTLKSTVEAIWAGIKATEAAVSEEFGLAPFLPDQIHFVHSQELLSRYPDLDAKGRERAIAKDLGAVFLVGIGGKLSDGHRHDVRAPDYDDWSTPSELGHAGLNGDILVWNPVLEDAFELSSMGIRVDADTLKHQLALTGDEDRLELEWHQALLRGEMPQTIGGGIGQSRLTMLLLQLPHIGQVQAGVWPAAVRESVPSLLN')
+            self.assertEqual(ss, '-HHHHHHHHHHHHHHHHHHHHHHH-EEE----SEEETTSS-S--TTTT----EE--SSSTT--EEE-S--TTHHHHHHHHTT--TT-EEEEEEEEE-TT-S---SS--SEEEEEEEEEE--TT--SHHHHHHHHHHHHHHHHHHHHHHHHHS-----S-SS-EEEEHHHHHHHSSSS-HHHHHHHHHHHHSEEEEE--SS--SSS--SS---TTTB--SSB-TTSSB-SEEEEEEEETTTTEEEEEEEEEEB--HHHHHHHHHHHT-TTGGGSHHHHHHHTT-S--EEEEEEEHHHHHHHHHT-S-GGGTS-----HHHHHH--S---')
+
+            self.assertEqual(len(sa), 328)
+            try:
+                np.testing.assert_allclose(sa[9:19], [0.23076923076923078,0.27918781725888325,0.0,0.13658536585365855,0.6153846153846154,0.16847826086956522,0.0,0.07692307692307693,0.5241935483870968,0.08080808080808081], rtol=1e-2)
+                np.testing.assert_allclose(sa[-2:], [0.35365853658536583,0.09554140127388536], rtol=1e-2)
+            except AssertionError:
+                self.fail('Failed SA for 12AS/A')
+
+            # TODO: test Phi/Psi values
+
+        tf.gfile.Remove( h5fname )
